f7f4379389e3012bdb945f1933ea3973bef09903
leensungzero <brianwoo119@naver.com>
2018-10-31 11:31:12 +0900

    UX 개선, CRUD함수 및 html 파일 정리 완료

--- crud.py
+++ crud.py
@@ -13,6 +13,13 @@ app.config.update(dict(
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    objects = User.query.all()
+
+    return render_template('index.html', objects=objects)
+
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
     form = UserForm(request.form)
     if request.method == 'POST':
         if form.validate():
@@ -20,12 +27,13 @@ def index():
             db.session.add(object)
             db.session.commit()
             print(object.id)
-            return redirect(url_for('create', pk=object.id))
+            return redirect(url_for('detail', pk=object.id))
     else:
-        return render_template('index.html', form=form)
+        return render_template('create.html', form=form)
+
 
 @app.route('/<int:pk>')
-def create(pk):
+def detail(pk):
     object = User.query.filter_by(id=pk).first()
 
     return render_template('detail.html', object=object)
@@ -38,18 +46,19 @@ def update(pk):
 
     # Todo: update 부분 수정하기
     if request.method == 'POST' and form.validate():
-        print(object.username, object.password)
-        print(form.username, form.password)
+        # print(object.username, object.password)
+        # print(form.username, form.password)
+        #
+        # object.username = form.username.data
+        # object.password = form.password.data
+        #
+        # print(object.username, object.password)
 
-        object.username = form.username.data
-        object.password = form.password.data
-
-        print(object.username, object.password)
-
+        # db.session.commit()
+        form.populate_obj(object)
         db.session.commit()
-        # form.populate_obj(object)
         # object.save()
-        return 'edit success'
+        return redirect(url_for('detail', pk=object.id))
     else:
         return render_template('update.html', form=form)
 
@@ -63,7 +72,7 @@ def delete(pk):
     db.session.delete(object)
     db.session.commit()
 
-    return 'delete success'
+    return render_template('delete_success.html', id=object.id)
 
 if __name__ == '__main__':
     app.run(debug=True)
